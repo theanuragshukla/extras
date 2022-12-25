297a1354e8c42e6089cdccc6b6f4d72ba01ca74d
theanuragshukla <tachyonanurag@gmail.com>
2022-12-26 02:29:40 +0530

    ..

---
+++ fish/functions/fish_greeting.fish
@@ -0,0 +1,6 @@
+function fish_greeting
+starship init fish | source  
+alias ls='exa --icons --no-user --group-directories-first  --time-style long-iso'
+alias ll='exa -l --icons --no-user --group-directories-first  --time-style long-iso'
+alias la='exa -l -a --icons --no-user --group-directories-first  --time-style long-iso'
+end
---
+++ fish/functions/fzf_key_bindings.fish
@@ -0,0 +1,1 @@
+/home/anurag/.fzf/shell/key-bindings.fish
